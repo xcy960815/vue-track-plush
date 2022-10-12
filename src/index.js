02b136a37b5b8c4067ed8793fa0934afc4f307e5
xuchongyu <xuchongyu@weidian.com>
2022-10-12 17:54:04 +0800

    .gitignore

--- src/index.js
+++ src/index.js
@@ -1,78 +1,78 @@
 // import Exposure from './exposure'
-import Click from './click'
-import Browse from './browse'
+import Click from "./click";
+import Browse from "./browse";
 
 // 忽略的字段
-const ignoreField = ["baseURL", "url"]
+const ignoreField = ["baseURL", "url"];
 
 // 指令 触发
 const install = function (Vue, trackPlushConfig = {}) {
-    Vue.directive('track', {
-        bind(el, binding, VNode) {
-            const {
-                arg
-            } = binding
+  Vue.directive("track", {
+    bind(el, binding, VNode) {
+      const { arg } = binding;
 
-            arg.split('|').forEach((item) => {
-                // 点击
-                if (item === 'click') {
-                    new Click(trackPlushConfig).handleClickEvent({
-                        el,
-                        VNode,
-                        type: 'instruction',
-                    })
-                }
+      arg.split("|").forEach((item) => {
+        // 点击
+        if (item === "click") {
+          new Click(trackPlushConfig).handleClickEvent({
+            el,
+            VNode,
+            type: "instruction",
+          });
+        }
 
-                // 曝光
-                // else if (item === 'exposure') {
-                //     new Exposure(trackPlushConfig).handleExposureEvent({
-                //         el,
-                //     })
-                // }
+        // 曝光
+        // else if (item === 'exposure') {
+        //     new Exposure(trackPlushConfig).handleExposureEvent({
+        //         el,
+        //     })
+        // }
 
-                // 浏览
-                else if (item === 'browse') {
-                    new Browse(trackPlushConfig).handleBrowseEvent({
-                        el,
-                        VNode,
-                        type: 'instruction',
-                    })
-                }
-            })
-        },
-    })
-}
+        // 浏览
+        else if (item === "browse") {
+          new Browse(trackPlushConfig).handleBrowseEvent({
+            el,
+            VNode,
+            type: "instruction",
+          });
+        }
+      });
+    },
+  });
+};
 
-if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
 
 // 点击事件
 export const clickEvent = (trackPlushConfig) => {
-    const clickEventParams = {}
-    Object.keys(trackPlushConfig).forEach((key) => {
-        if (!ignoreField.includes(key)) clickEventParams[key] = trackPlushConfig[key]
-    })
+  const clickEventParams = {};
+  Object.keys(trackPlushConfig).forEach((key) => {
+    if (!ignoreField.includes(key))
+      clickEventParams[key] = trackPlushConfig[key];
+  });
 
-    new Click(trackPlushConfig).handleClickEvent({
-        ...clickEventParams,
-        type: 'customize',
-    })
-}
+  new Click(trackPlushConfig).handleClickEvent({
+    ...clickEventParams,
+    type: "customize",
+  });
+};
 
 // 浏览事件
 export const browseEvent = (trackPlushConfig) => {
-    const browseEventParams = {}
-    Object.keys(trackPlushConfig).forEach((key) => {
-        if (!ignoreField.includes(key)) browseEventParams[key] = trackPlushConfig[key]
-    })
+  const browseEventParams = {};
+  Object.keys(trackPlushConfig).forEach((key) => {
+    if (!ignoreField.includes(key))
+      browseEventParams[key] = trackPlushConfig[key];
+  });
 
-    new Browse(trackPlushConfig).handleBrowseEvent({
-        ...browseEventParams,
-        type: 'customize',
-    })
-}
+  new Browse(trackPlushConfig).handleBrowseEvent({
+    ...browseEventParams,
+    type: "customize",
+  });
+};
 
 export default {
-    install,
-}
+  install,
+};
